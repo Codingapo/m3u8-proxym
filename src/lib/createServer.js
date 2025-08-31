0507ebc9a67dcfd3ea947e6b334e2edf22687093
Codingapo <124777804+Codingapo@users.noreply.github.com>
2025-08-31 07:49:20 +0200

    Update createServer.js

--- src/lib/createServer.js
+++ src/lib/createServer.js
@@ -1,25 +1,21 @@
 import getHandler from "./getHandler.js";
 import httpProxy from "http-proxy";
 import http from "node:http";
+import https from "node:https";
 
-export default function createServer(options) {
-  options = options || {};
-
+export default function createServer(options = {}) {
+  // Merge default http-proxy options with custom options
   const httpProxyOptions = {
     xfwd: true,
     secure: process.env.NODE_TLS_REJECT_UNAUTHORIZED !== "0",
+    ...options.httpProxyOptions,
   };
-
-  if (options.httpProxyOptions) {
-    Object.keys(options.httpProxyOptions).forEach(function (option) {
-      httpProxyOptions[option] = options.httpProxyOptions[option];
-    });
-  }
 
   const proxyServer = httpProxy.createProxyServer(httpProxyOptions);
   const requestHandler = getHandler(options, proxyServer);
   let server;
 
+  // CORS handler
   const handleCors = (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -31,7 +27,7 @@ export default function createServer(options) {
       "Content-Type, Authorization"
     );
     res.setHeader("Access-Control-Allow-Credentials", "true");
-  
+
     if (req.method === "OPTIONS") {
       res.writeHead(204);
       res.end();
@@ -40,71 +36,66 @@ export default function createServer(options) {
     return false;
   };
 
+  // Origin check
   const isOriginAllowed = (origin, options) => {
-    if (options.originWhitelist.includes("*")) {
-      return true;
-    }
+    // Allow empty origin (direct browser requests)
+    if (!origin) return true;
+
+    if (options.originWhitelist.includes("*")) return true;
     if (
       options.originWhitelist.length &&
       !options.originWhitelist.includes(origin)
-    ) {
+    )
       return false;
-    }
     if (
       options.originBlacklist.length &&
       options.originBlacklist.includes(origin)
-    ) {
+    )
       return false;
-    }
+
     return true;
   };
 
+  // Unified request listener for HTTP & HTTPS
+  const requestListener = (req, res) => {
+    const origin = req.headers.origin || "";
+
+    if (!isOriginAllowed(origin, options)) {
+      res.writeHead(403, "Forbidden");
+      res.end(
+        `The origin "${origin}" was blacklisted by the operator of this proxy.`
+      );
+      return;
+    }
+
+    if (handleCors(req, res)) return;
+
+    requestHandler(req, res);
+  };
+
   if (options.httpsOptions) {
-    server = https.createServer(options.httpsOptions, (req, res) => {
-      const origin = req.headers.origin || "";
-      if (!isOriginAllowed(origin, options)) {
-        res.writeHead(403, "Forbidden");
-        res.end(
-          `The origin "${origin}" was blacklisted by the operator of this proxy.`
-        );
-        return;
-      }
-      if (handleCors(req, res)) return;
-      requestHandler(req, res);
-    });
+    server = https.createServer(options.httpsOptions, requestListener);
   } else {
-    server = http.createServer((req, res) => {
-      const origin = req.headers.origin || "";
-      if (!isOriginAllowed(origin, options)) {
-        res.writeHead(403, "Forbidden");
-        res.end(
-          `The origin "${origin}" was blacklisted by the operator of this proxy.`
-        );
-        return;
-      }
-      if (handleCors(req, res)) return;
-      requestHandler(req, res);
-    });
+    server = http.createServer(requestListener);
   }
 
-  proxyServer.on("error", function (err, req, res) {
+  // Proxy error handler
+  proxyServer.on("error", (err, req, res) => {
     console.error("Proxy error:", err);
+
     if (res.headersSent) {
-      if (!res.writableEnded) {
-        res.end();
-      }
+      if (!res.writableEnded) res.end();
       return;
     }
 
     const headerNames = res.getHeaderNames
       ? res.getHeaderNames()
       : Object.keys(res._headers || {});
-    headerNames.forEach(function (name) {
-      res.removeHeader(name);
-    });
 
-    res.writeHead(404, { "Access-Control-Allow-Origin": "*" });
-    res.end("Not found because of proxy error: " + err);
+    headerNames.forEach((name) => res.removeHeader(name));
+
+    res.writeHead(500, { "Access-Control-Allow-Origin": "*" });
+    res.end("Proxy error: " + (err.message || err));
   });
 
   return server;
